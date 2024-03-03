280ce8d04522e98bbdc9477051b6b593de22fa1d
Femvan <155897044+Femvan@users.noreply.github.com>
2024-03-03 12:33:01 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -51,10 +51,6 @@ Bot = Client(
     bot_token=Config.BOT_TOKEN,
     api_id=Config.API_ID,
     api_hash=Config.API_HASH
-    app = web.AppRunner(await web_server())
-    await app.setup()
-    bind_address = "0.0.0.0"
-    await web.TCPSite(app, bind_address, PORT).start()
 )
 
 
